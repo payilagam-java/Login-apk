573850014585bc03630d548d637af104fcf133a7
Ayyappanstark <rockthe166@gmail.com>
2024-06-24 15:03:37 +0530

    added url

--- app/src/main/java/com/example/splash/MainActivity.java
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.splash;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +16,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText username;
+    EditText password;
+    Button loginButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(username.getText().toString().equals("user") && password.getText().toString().equals("1234")){
+                    Toast.makeText(MainActivity.this, "login Successful", Toast.LENGTH_SHORT).show();
+                    // Redirect to the specified URL
+                    String url = "http://arivukarasu-002-site1.ltempurl.com/api/AccessToken/GetAccessToken";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
